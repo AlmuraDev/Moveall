b5211b0709f013dc3ef2056702aaaca2034d1c93
Steven Downer <grinch@outlook.com>
2013-04-11 14:31:46 -0500

    Small cleanup Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/com/almuradev/sortstack/SortStackPlugin.java
+++ src/main/java/com/almuradev/sortstack/SortStackPlugin.java
@@ -17,30 +17,12 @@
  * You should have received a copy of the GNU General Public License. If not,
  * see <http://www.gnu.org/licenses/> for the GNU General Public License.
  */
-/*
- * This file is part of Realms.
- *
- * © 2013 AlmuraDev <http://www.almuradev.com/>
- * Realms is licensed under the GNU General Public License.
- *
- * Realms is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Realms is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License. If not,
- * see <http://www.gnu.org/licenses/> for the GNU General Public License.
- */
 package com.almuradev.sortstack;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public class SortStackPlugin extends JavaPlugin {
 
@@ -54,8 +36,12 @@ public class SortStackPlugin extends JavaPlugin {
 	public void onEnable() {
 		super.onEnable();
 		instance = this;
-		FileConfiguration config = this.getConfig();
-		saveConfig();
+
+        // Read in default config.yml
+        if (!new File(this.getDataFolder(), "config.yml").exists()) {
+            this.saveDefaultConfig();
+        }
+
 		PluginManager pm = this.getServer().getPluginManager();
 		pm.registerEvents(new SortStackListener(this), this);
 			
